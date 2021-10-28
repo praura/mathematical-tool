13078d385b66e36d20d28d1aaf51c1f93233dcff
praura <85402529+praura@users.noreply.github.com>
2021-10-28 21:09:09 +0300

    Add files via upload

--- Rules.h
+++ Rules.h
@@ -4,66 +4,66 @@
 #include <string>
 
 void instruction () {
-    std::cout << "Welcome to mathematical tool. You can find any mathematical formulas and type your equations" << std::endl
-              << "                |                " << std::endl
-              << "               |/                " << std::endl
-              << "Firstly you should write type of your request: quadratic equation(QUADequation), equation (equation), formula(formula) or info(info) " << std::endl;
+    CONSOLE_OUT << "Welcome to mathematical tool. You can find any mathematical formulas and type your equations" << END_LINE
+              << "                |                " << END_LINE
+              << "               |/                " << END_LINE
+              << "Firstly you should write type of your request: quadratic equation(QUADequation), equation (equation), formula(formula) or info(info) " << END_LINE;
 
     str type, command;
     str first_letter, name_of_figure;
     command.resize(9);
-    std::cout << "Me: ";
-    std::cin >> type;
+    CONSOLE_OUT << "Me: ";
+    INPUT >> type;
     while (true) {
         if (type == "info") {
-            std::cout << "To see all mathematical operations you can use, type '/mall' " << std::endl
-                      << "If you want to write your equation, type /eq " << std::endl
-                      << "If you want to find formulas, type '/formulas' for detail info" << std::endl;
-            std::cout << std::endl;
-            std::cout << "Me: ";
-            std::cin >> command;
+            CONSOLE_OUT << "To see all mathematical operations you can use, type '/mall' " << END_LINE
+                      << "If you want to write your equation, type /eq " << END_LINE
+                      << "If you want to find formulas, type '/formulas' for detail info" << END_LINE;
+            CONSOLE_OUT << END_LINE;
+            CONSOLE_OUT << "Me: ";
+            INPUT >> command;
             if (command == "/mall") {
-                std::cout << std::endl;
-                std::cout << "example : sum a b ->  a + b " << std::endl
-                          << "example : abs a -> |a| " << std::endl
-                          << "example : dif a b -> a - b " << std::endl
-                          << "example : mul a b -> a * b " << std::endl
-                          << "example : << a b -> a << b  " << std::endl
-                          << "example : power a b -> a ^ b " << std::endl
+                CONSOLE_OUT << END_LINE;
+                CONSOLE_OUT << "example : sum a b ->  a + b " << END_LINE
+                          << "example : abs a -> |a| " << END_LINE
+                          << "example : dif a b -> a - b " << END_LINE
+                          << "example : mul a b -> a * b " << END_LINE
+                          << "example : << a b -> a << b  " << END_LINE
+                          << "example : power a b -> a ^ b " << END_LINE
                           << "-------------------------------------";
-                std::cout << std::endl;
-                std::cout << "abs a - |a|" << std::endl
-                          << "sqrt a - quadratic root of a" << std::endl
-                          << "pow a n - a ^ n" << std::endl
-                          << "sum a b - a + b" << std::endl
-                          << "dif a b - a - b" << std::endl
-                          << "mul a b - a * b" << std::endl
-                          << "div a b - a / b" << std::endl
-                          << "mod a b - a % b" << std::endl;
-                std::cout << std::endl;
+                CONSOLE_OUT << END_LINE;
+                CONSOLE_OUT << "abs a - |a|" << END_LINE
+                          << "sqrt a - quadratic root of a" << END_LINE
+                          << "pow a n - a ^ n" << END_LINE
+                          << "sum a b - a + b" << END_LINE
+                          << "dif a b - a - b" << END_LINE
+                          << "mul a b - a * b" << END_LINE
+                          << "div a b - a / b" << END_LINE
+                          << "mod a b - a % b" << END_LINE;
+                CONSOLE_OUT << END_LINE;
             } else if (command == "/formulas") {
-                std::cout << std::endl;
-                std::cout << "example : V cube " << std::endl
-                          << "example : S parallelepiped " << std::endl;
-                std::cout << std::endl;
+                CONSOLE_OUT << END_LINE;
+                CONSOLE_OUT << "example : V cube " << END_LINE
+                          << "example : S parallelepiped " << END_LINE;
+                CONSOLE_OUT << END_LINE;
             }
             if (command == "/eq")
             {
-                std::cout << "Enter your equation" << std::endl
-                          << "If you will want to exit, type /end" << std::endl;
+                CONSOLE_OUT << "Enter your equation" << END_LINE
+                          << "If you will want to exit, type /end" << END_LINE;
                 break;
             }
 
             else if (command != "/mall" && command != "/eq" && command != "/formulas" && command != "/end"){
-                std::cout << "You wrote incorrect command type, try to rewrite it" << std::endl;
-                std::cout << std::endl;
+                CONSOLE_OUT << "You wrote incorrect command type, try to rewrite it" << END_LINE;
+                CONSOLE_OUT << END_LINE;
             }
 
         }
         else if (type == "equation")
         {
-            std::cout << "Enter your equation" << std::endl
-                      << "If you will want to exit, type /end" << std::endl;
+            CONSOLE_OUT << "Enter your equation" << END_LINE
+                      << "If you will want to exit, type /end" << END_LINE;
             break;
         }
         if (type == "QUADequation")
@@ -76,49 +76,49 @@ void instruction () {
         }
         else if (type == "formula")
         {
-            std::cin >> first_letter >> name_of_figure;
+            INPUT >> first_letter >> name_of_figure;
             if(first_letter == "S" || first_letter == "s")
             {
                 if (name_of_figure == "rectangle")
                 {
-                    std::cout << " = a * b";
+                    CONSOLE_OUT << " = a * b";
                 }
                 else if (name_of_figure == "square")
                 {
-                    std::cout << " = a * a";
+                    CONSOLE_OUT << " = a * a";
                 }
                 if (name_of_figure == "arbitrary_triangle")
                 {
-                    std::cout << " = 1/2 * a * h" << std::endl
-                    << " = 1/2 * a * b * sin(z)" << std::endl
-                    << " = sqrt(p * (p - a)*(p - b)*(p - c) )" << std::endl
-                    << " = a * b * c / 4 * R " << std::endl
-                    << " = r * p" << std::endl;
+                    CONSOLE_OUT << " = 1/2 * a * h" << END_LINE
+                    << " = 1/2 * a * b * sin(z)" << END_LINE
+                    << " = sqrt(p * (p - a)*(p - b)*(p - c) )" << END_LINE
+                    << " = a * b * c / 4 * R " << END_LINE
+                    << " = r * p" << END_LINE;
                 }
                 else if (name_of_figure == "right_triangle")
                 {
-                    std::cout << " = 1/2 * a * b" << std::endl
-                    << " = 1/2 * BA * CA * sin(A)" << std::endl;
+                    CONSOLE_OUT << " = 1/2 * a * b" << END_LINE
+                    << " = 1/2 * BA * CA * sin(A)" << END_LINE;
                 }
                 if (name_of_figure == "equilateral_triangle")
                 {
-                    std::cout << " = (a ^ 2 * sqrt(3) ) / 4";
+                    CONSOLE_OUT << " = (a ^ 2 * sqrt(3) ) / 4";
                 }
                 else if (name_of_figure == "arbitrary_quadrilateral")
                 {
-                    std::cout << " = 1/2 * d1 * d2 * sin(z)";
+                    CONSOLE_OUT << " = 1/2 * d1 * d2 * sin(z)";
                 }
                 if (name_of_figure == "parallelogram")
                 {
-                    std::cout << " = a * h" << std::endl
-                    << " = a * b * sin(z)" << std::endl
-                    << " = 1/2 * d1 * d2 * sin(z)" << std::endl;
+                    CONSOLE_OUT << " = a * h" << END_LINE
+                    << " = a * b * sin(z)" << END_LINE
+                    << " = 1/2 * d1 * d2 * sin(z)" << END_LINE;
                 }
                 else if (name_of_figure == "trapeze")
                 {
-                    std::cout << " = ( (a + b) / 2 ) * h" << std::endl;
-                    std::cout << " = a * h" << std::endl
-                              << " = 1/2 * d1 * d2 * sin(z)" << std::endl;
+                    CONSOLE_OUT << " = ( (a + b) / 2 ) * h" << END_LINE;
+                    CONSOLE_OUT << " = a * h" << END_LINE
+                              << " = 1/2 * d1 * d2 * sin(z)" << END_LINE;
                 }
             }
         }
---
+++ def.h
@@ -0,0 +1,6 @@
+#define CONSOLE_OUT std::cout
+#define END_LINE std::endl
+#define INPUT std::cin
+
+typedef std::string str;
+typedef double dbl;
